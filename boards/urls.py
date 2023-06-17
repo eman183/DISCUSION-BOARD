fe521ee42a9f58fd024e588664a1d04527e7b961
eman183 <eman.mohamed1832000@gmail.com>
2023-06-17 21:27:19 +0300

    create database models and forms

--- boards/urls.py
+++ boards/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("about",views.about,name="about"),
+    path("boards/<int:board_id>",views.board_topics,name="board_topics"),
+    path("boards/<int:board_id>/new",views.new_topic,name="new_topic")
+
+
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Board
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,Http404
+from .models import Board,Topic,Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
    boards=Board.objects.all()
-   board_names=[]
-   for board in boards:
-       board_names.append(board.name)
-       return HttpResponse(f"{board_names}")
+
+   return render(request,"boards/home.html",{"boards":boards})
+
+
+
+def about(request):
+   return HttpResponse("hallo from about")
+
+
+def board_topics(request,board_id):
+#    try:
+#        board=Board.objects.get(id=board_id)
+#    except Board.DoesNotExist:
+#           raise Http404
+   board=get_object_or_404(Board,id=board_id)
+   return render(request,"boards/topics.html",{"board":board})
+
+
+
+def new_topic(request,board_id):
+      board=get_object_or_404(Board,id=board_id)
+      print(board)
+      if request.method=="POST":
+          subject=request.POST['subject']
+          massage=request.POST['message']
+          print(subject)
+          print(massage)
+          user=User.objects.first()
+          topic=Topic.objects.create(
+              subject=subject,
+              board=board,
+              created_by=user)
+          post=Post.objects.create(
+              massage=massage,
+              topic=topic,
+              created_by=user)
+          return redirect("board_topics",board_id=board.pk)
+      return render(request,"boards/new_topic.html",{"board":board})
