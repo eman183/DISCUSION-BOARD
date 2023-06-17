93532d0a5051e6afaa9b598ee5ffa59b5707ff00
eman183 <eman.mohamed1832000@gmail.com>
2023-06-17 16:44:56 +0300

    create database models

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Board(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    discription=models.CharField(max_length=150)
+
+
+
+
+class Topic(models.Model):
+    subject=models.CharField(max_length=250)
+    board=models.ForeignKey(Board,related_name='topics',on_delete=models.CASCADE)    
+    created_by=models.ForeignKey(User,related_name="topics",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+
+class Post(models.Model):
+    massage=models.TextField(max_length=4000)
+    topic=models.ForeignKey(Topic,related_name="posts",on_delete=models.CASCADE)
+    created_by=models.ForeignKey(User,related_name="posts",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+   
+   
